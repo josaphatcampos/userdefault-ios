d9710020c6a04f10ea0660a6e407460117a88c06
Josaphat Campos <josaphat.campos@yahoo.com.br>
2023-03-30 00:53:17 -0300

    first commit

--- userdefault/AppDelegate.swift
+++ userdefault/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        firstlounch()
         return true
     }
 
@@ -29,6 +30,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func firstlounch(){
+        if UserDefaults.standard.bool(forKey: "hasLauchedBefore") {
+            print("O aplicativo já foi inicializado antes")
+        }else{
+            print("O aplicativo não foi inicializado antes")
+            UserDefaults.standard.set(true, forKey: "hasLauchedBefore")
+            UserDefaults.standard.set(false, forKey: "isDark")
+        }
     }
 
 
--- userdefault/ViewController.swift
+++ userdefault/ViewController.swift
@@ -8,10 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var themeSwitch: UISwitch!
+    
+    let themekey:String = "isDark"
+    
+    let defaults = UserDefaults.standard
+    
+    @IBAction func switchFlipped(sender:Any){
+        defaults.set(themeSwitch.isOn, forKey: themekey)
+        changeTheme(themeSwitch.isOn)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let isDark = defaults.value(forKey: themekey){
+            if isDark as! Bool{
+                changeTheme(isDark as! Bool)
+            }
+        }
+        
+        sandBoxFIle()
+    }
+    
+    func changeTheme(_ value:Bool){
+        themeSwitch.setOn(value, animated: false)
+        overrideUserInterfaceStyle = value ? .dark : .light
+    }
+    
+    func sandBoxFIle(){
+        let fileManager = FileManager.default
+        let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        let url = urls.last?.appendingPathComponent("file.txt")
+        
+        do{
+            try "Olá Mundo!".write(to: url!, atomically: true, encoding: .utf8)
+        }catch{
+            print("Erro ao tentar escrever no arquivo")
+        }
+        
+        do{
+            let content = try String(contentsOf: url!, encoding: String.Encoding.utf8)
+            
+            if content == "Olá Mundo!"{
+                print("Deu certo \(content)")
+            }else{
+                print("nao")
+            }
+        }catch{
+            print("Não conseguiu ler conteudo")
+        }
     }
 
 
